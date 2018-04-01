0c9b254c0d2195828cb7b89f5718f7a6cb0d6cfe
strangerintheq <tripmannn@gmail.com>
2018-04-01 18:18:41 +0300

    текст и картинки

--- js/pages.js
+++ js/pages.js
@@ -6,15 +6,19 @@
     req.open('get', 'pages.txt', true);
     req.onreadystatechange = function () {
         if (req.readyState === 4 && req.status === 200) {
-            var html = '<div class="container"><div class="row"><div class="col-lg-8 col-md-10 mx-auto">';
+            var html = ''
             req.responseText.split('---').forEach(function (postText, i) {
+                html += html ? '<hr>' : '<div class="container"><div class="row"><div class="col-lg-8 col-md-10 mx-auto">';
                 var post = postText.split('\n');
                 var title = '';
                 while (+title === +'')
                     title = post.shift();
+                var text = '';
+                while (text.length < 100)
+                    text += post.shift();
                 html += '<div class="post-preview"><a href="post.html?'+i+'"><h2 class="post-title">'
                     + title + '</h2><h3 class="post-subtitle">'
-                    + post.join('') + '</h3></a></div><hr>'
+                    + text + ' ...</h3></a></div>'
             });
             target.innerHTML = html + '</div></div></div>';
         }
