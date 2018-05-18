8b200a915761e34e03601970d35bfa92f21fe849
Balashev Konstantin <balashev.konstantin@nicetu.spb.ru>
2018-05-18 17:58:09 +0300

    new posts format read

--- js/page.js
+++ js/page.js
@@ -1,27 +1,27 @@
 (function () {
+
+    var id = document.location.search.substring(1);
+
     var target = document.createElement('div');
     document.body.appendChild(target);
     var req = new XMLHttpRequest();
-    req.open('get', 'pages.txt', true);
+    req.open('get', 'posts/' + id + '.json', true);
     req.onreadystatechange = function () {
         if (req.readyState === 4 && req.status === 200) {
             var html = '';
-            var index = document.location.search.substring(1);
-            var post = req.responseText.split('---')[index].split('\n');
-            var title = '';
-            while (+title === +'')
-                title = post.shift();
+
+            var post = JSON.parse(req.responseText);
 
             html += '<header class="masthead" style="background-image: url(\'img/post-bg.jpg\')">' +
                 '<div class="overlay"></div><div class="container"><div class="row">' +
                 '<div class="col-lg-8 col-md-10 mx-auto"><div class="post-heading">' +
-                '<h2>' + title + '</h2>' +
+                '<h2>' + post.title + '</h2>' +
                 '</div></div></div></div></header>' +
 
                 '<article><div class="container"><div class="row">' +
                 '<div class="col-lg-8 col-md-10 mx-auto">' +
                 '<h2 class="section-heading"></h2>' +
-                '<p>' + post.join('') + '</p>' +
+                '<p>' + post.text + '</p>' +
                 '</div></div></div></article>';
 
             target.innerHTML = html;
--- js/pages.js
+++ js/pages.js
@@ -1,27 +1,37 @@
 (function () {
-    var target = document.createElement('div');
-    target.innerHTML = '<div class="container"><div class="row"><div class="col-lg-8 col-md-10 mx-auto">';
-    document.body.appendChild(target);
-    var req = new XMLHttpRequest();
-    req.open('get', 'pages.txt', true);
-    req.onreadystatechange = function () {
-        if (req.readyState === 4 && req.status === 200) {
-            var html = ''
-            req.responseText.split('---').forEach(function (postText, i) {
-                html += html ? '<hr>' : '<div class="container"><div class="row"><div class="col-lg-8 col-md-10 mx-auto">';
-                var post = postText.split('\n');
-                var title = '';
-                while (+title === +'')
-                    title = post.shift();
-                var text = '';
-                while (text.length < 100)
-                    text += post.shift();
-                html += '<div class="post-preview"><a href="post.html?'+i+'"><h4 >'
-                    + title + '</h4><h5 class="post-subtitle">'
-                    + text + ' ...</h5></a></div>'
-            });
-            target.innerHTML = html + '</div></div></div>';
-        }
-    };
-    req.send();
+
+    var target = div('', document.body);
+    var container = div('container', target);
+    var row = div('row', container);
+    var col = div('col-lg-8 col-md-10 mx-auto', row);
+
+    req('posts/index.json', function (data) {
+        data.ids.forEach(function (id) {
+            col.innerHTML && col.appendChild(document.createElement('hr'));
+            var postBody = div('post-preview', col);
+            req('posts/' + id + '.json', function (postData) {
+                postBody.innerHTML = '<div class="post-preview"><a href="post.html?'+id+'"><h4 >'
+                + postData.title + '</h4><h5 class="post-subtitle">'
+                + postData.text.substring(0, 100) + ' ...</h5></a></div>'
+            })
+        });
+    });
+
+    function req(url, callback) {
+        var r = new XMLHttpRequest();
+        r.open('get', url, true);
+        r.onreadystatechange = function () {
+            if (r.readyState === 4 && r.status === 200) {
+                callback(JSON.parse(r.responseText));
+            }
+        };
+        r.send();
+    }
+
+    function div(className, parent) {
+        var el = document.createElement('div');
+        el.className = className;
+        parent.appendChild(el);
+        return el;
+    }
 })();
