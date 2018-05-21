6f7332e41d9b8ae21b072ac6cf49c0ab46bf4e86
Balashev Konstantin <balashev.konstantin@nicetu.spb.ru>
2018-05-21 14:13:59 +0300

    posts sync

--- sync/sync.js
+++ sync/sync.js
@@ -1,49 +1,101 @@
 var fs;
-var actual;
+var actual = [];
+var remain;
 
-readActualPosts(initVk);
+chain(
+    initVk,
+    initFS,
+    gitClone,
+    processWallPosts,
+    updateIndexFile,
+    gitAdd,
+    gitCommit,
+    gitPush,
+    done
+);
 
-function readActualPosts(callback) {
-    log('reading actual posts:');
-    req('../posts/index.json', function (postsIndex) {
-        actual = postsIndex.ids;
-        log('actual posts:' + actual.length);
-        callback()
-    });
+function done() {
+    log('done')
 }
 
-function preparePostIds(postsListData) {
-    return postsListData.response.items.filter(function (post) {
-        return actual.indexOf(post.id) === -1;
-    }).map(function (post) {
-        return post.owner_id + '_' + post.id
-    });
+function gitPush(finish) {
+    git.push({
+        fs: fs,
+        dir: '/',
+        remote: 'origin',
+        ref: 'master',
+        authUsername: 'strangerintheq',
+        authPassword: 'KlopKlop1'
+    }).then(finish)
 }
 
-function getAllPosts() {
+function gitCommit(finish) {
+    git.commit({
+        fs: fs,
+        dir: '/',
+        author: {
+            name: 'wall sync script',
+            email: 'mrtest@example.com'
+        },
+        message: 'vk wall sync'
+    }).then(finish);
+}
+
+function gitAdd(file, callback) {
+    git.add({
+        fs: fs,
+        dir: '/',
+        filepath: file
+    }).then(callback);
+}
+
+function processWallPosts(finish) {
     log('getting posts from wall');
-    call('wall.get', function (postsListData) {
-        // log(postsListData)
-        log('total posts: ' + postsListData.response.count);
-        preparePostIds(postsListData).forEach(updatePost);
-        // updatePost(preparePostIds(postsListData)[0])
-    })
+    vkApiCall('wall.get', function (postsListData) {
+        var total = postsListData.response.count;
+        remain = total;
+        log('total posts: ' + total);
+        log('');
+        asyncLoop(processChunk, 0, total, 100, 1000);
+    });
+
+    function processChunk(i) {
+        vkApiCall('wall.get', {
+            count: 100,
+            offset: i
+        }, function (posts) {
+            posts.response.items.forEach(updatePost)
+        });
+    }
+
+    function updatePost(post) {
+        actual.push(post.id);
+        var file = '/posts/' + post.id + '.json';
+        fs.writeFile(file, post.text, 'utf8', function () {
+            gitAdd(file, fileWritten);
+        });
+    }
+
+    function fileWritten() {
+        remain -= 1;
+        removeLastLogRecord();
+        log('remain ' + remain);
+        if (remain > 0)
+            return;
+        removeLastLogRecord();
+        log('updating posts complete, processed: ' + actual.length);
+        finish();
+    }
 }
 
-function updatePost(postId) {
-    call('wall.getById', {
-        posts: [postId]
-    }, function (postsContents) {
-        var text = postsContents.response[0].text;
-        log(text);
-
-        fs.writeFile('posts/' + postId + '.json', text, 'utf8').then(function () {
-
-        })
-    })
+function updateIndexFile(finish) {
+    var file = 'posts/index.json';
+    fs.writeFile(file, JSON.stringify(actual), 'utf8', function () {
+        gitAdd(file, finish);
+    });
 }
 
-function gitClone() {
+function gitClone(finish) {
     var repo = 'art-therapy/art-therapy.github.io';
     log('cloning github repository ' + repo);
 
@@ -57,47 +109,65 @@ function gitClone() {
 
     function success() {
         log('clone success');
-        // readFile(fs);
-        getAllPosts();
+        finish();
     }
 }
 
-function initVk() {
-    var vkProps = { apiId: '5043774' };
-    log('vk api: ' + vkProps.apiId);
-    VK.init(vkProps);
-    vkLogin();
-}
-
-function vkLogin() {
+function initVk(finish) {
+    VK.init({ apiId: '5043774' });
     log('vk login');
     VK.Auth.login(function () {
         log('vk login success');
-        initFS(gitClone);
+        finish();
     });
 }
 
-function initFS(fsLoadCallback) {
-    var fsConfig = {
+function initFS(finish) {
+    BrowserFS.configure({
         fs: "IndexedDB",
         options: {}
-    };
-
-    BrowserFS.configure(fsConfig, function (err) {
+    }, function (err) {
         if (err)
             return console.error(err);
         fs = BrowserFS.BFSRequire("fs");
-        fsLoadCallback();
+        log('filesystem initialized');
+        finish();
     });
 }
 
-function call(method, paramsOrCallback, callback) {
+function vkApiCall(method, paramsOrCallback, callback) {
     var params = callback ? paramsOrCallback : {};
     params.v = '5.74';
     callback = callback || paramsOrCallback;
     VK.Api.call(method, params, callback)
 }
 
+function chain() {
+
+    var queue = Array.prototype.slice.call(arguments);
+
+    run();
+
+    function run() {
+        queue.shift()(run);
+    }
+}
+
+function asyncLoop(func, from, to, step, timeout) {
+    var i = from;
+    doStep(i);
+
+    function doStep() {
+        func(i);
+        i += step;
+        i < to && setTimeout(doStep, timeout);
+    }
+}
+
 function log(a) {
     document.body.appendChild(htmlify(a));
+}
+
+function removeLastLogRecord() {
+    document.body.removeChild(document.body.lastChild);
 }
